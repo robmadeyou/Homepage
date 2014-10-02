3ff7e98df21895951c6d6ae9cf05945fbfe3ed60
robmadeyou <robmadeyou@gmail.com>
2014-10-03 00:46:50 +0100

    Songs get pulled; saved to DB, renamed! Yay

--- pull/index.php
+++ pull/index.php
@@ -46,8 +46,8 @@ require_once( '../mysql/Mysql.php' );
 	$mysql = new Mysql();
 
 	$url = str_replace( ";", "", $url );
-	$output = shell_exec( "cd ../tmp/; youtube-dl -x --audio-quality 0 -i --add-metadata --write-thumbnail --prefer-avconv -o '%(id)s |!| %(uploader)s |!| %(title)s.%(ext)s' " . $url );
-
+	$output = shell_exec( "youtube-dl -x --audio-quality 0 -i --add-metadata --write-thumbnail --prefer-avconv -o '../tmp/%(id)s !i! %(uploader)s !i! %(title)s.%(ext)s' " . $url );
+    print $output;
 	$directory = scandir( "../tmp/" );
 	foreach( $directory as $item )
 	{
@@ -56,9 +56,11 @@ require_once( '../mysql/Mysql.php' );
 			$info = pathinfo( $item );
 			if( $info[ "extension" ] == "mp3" || $info[ "extension" ] == "m4a" )
 			{
-				$songExploded = explode( " !|! ", $item );
-				$songUrl = $songExploded[0];
-				$uploader = $songExploded[1];
+				$songExploded = explode( " !i! ", $item );
+                $songName = $songExploded[ 2 ];
+                $image = str_replace( $info[ "extension" ], "jpg", $songName );
+				$songUrl = $songExploded[ 0 ];
+				$uploader = $songExploded[ 1 ];
 
 				$mysql->InsertSong(
 					[
@@ -68,15 +70,21 @@ require_once( '../mysql/Mysql.php' );
 						"custom_name",
 						"ip",
 						"uploader",
+                        "notes",
+                        "image"
 					],
 					[
 						mysqli_real_escape_string( $mysql->GetMysqli(), $url ),
-						mysqli_real_escape_string( $mysql->GetMysqli(), $item ),
+						mysqli_real_escape_string( $mysql->GetMysqli(), $songName ),
 						mysqli_real_escape_string( $mysql->GetMysqli(), $tags ),
 						mysqli_real_escape_string( $mysql->GetMysqli(), $customPrefix ),
 						mysqli_real_escape_string( $mysql->GetMysqli(), $_SERVER['REMOTE_ADDR'] ),
 						mysqli_real_escape_string( $mysql->GetMysqli(), $uploader ),
+                        mysqli_real_escape_string( $mysql->GetMysqli(), $notes ),
+                        mysqli_real_escape_string( $mysql->GetMysqli(), $image )
 					]);
+                rename( '../tmp/' . $item, $songName );
+                rename( '../tmp/' . str_replace( $info[ "extension" ], "jpg", $item ), $image );
 			}
 		}
 	}
