e9bcfacd277773c351b8c4a763c1566ada91726f
robmadeyou <robmadeyou@gmail.com>
2014-12-31 21:52:09 +0000

    Lol I lied, this time it will work ;)

--- judge/index.php
+++ judge/index.php
@@ -7,7 +7,7 @@ require_once( '../song/Song.php' );
 			<script type="text/javascript" src="http://code.createjs.com/easeljs-0.7.1.min.js"></script>
 			<script type="text/javascript" src="http://code.createjs.com/soundjs-0.5.2.min.js"></script>
 			<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-			<link rel="stylesheet" href="static/style.css">
+			<link rel="stylesheet" href="../static/style.css">
 			<title id="title">Welcome</title>
 			<link rel="shortcut icon" href="http://www.localfoodmap.net/images/icons/banana.ico"/>
 		</head>
@@ -19,7 +19,7 @@ require_once( '../song/Song.php' );
 				$mysql = new Mysql();
 				foreach( $mysql->GetSongList() as $song )
 				{
-					print '<a href="../?a='. $song[ "id" ] .'"><img class="floatingImages" src="music/' . $song[ "image" ] . '" /></a>';
+					print '<a href="../?a='. $song[ "id" ] .'"><img class="floatingImages" src="../music/' . $song[ "image" ] . '" /></a>';
 				}
 			?>
 		</body>
