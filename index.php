529160b940d13b3df95cd08cbd63f361f7bdcd59
robmadeyou <robmadeyou@gmail.com>
2015-01-03 23:06:34 +0000

    Put everything in one line on the title bar, might need to add some styling

--- index.php
+++ index.php
@@ -47,12 +47,12 @@ if( isset( $_COOKIE ) && isset( $_COOKIE[ "user" ] ) )
 		</div>
 		<div id="holder">
 			<div id="titleBar">
-				<p>
-				<?=
-					$loggedIn ? "You're logged in! <span id=\"logout\">Logout</span>" : "You're not logged in yet <span id=\"login\">Log in</span>";
-				?>
-				</p>
-				<p id="songTitle"></p><button type="button" id="nextButton" onclick="playNextSong()" >Next song!</button> <input id="volume" type="range" min="0" max="100" onchange="changeVolume( this.value )">
+				<span id="songTitle"></span>
+				<span><?=
+						$loggedIn ? "You're logged in! <span id=\"logout\">Logout</span>" : "You're not logged in yet <span id=\"login\">Log in</span>";
+					?>
+				</span>
+				<button type="button" id="nextButton" onclick="playNextSong()" >Next song!</button> <input id="volume" type="range" min="0" max="100" onchange="changeVolume( this.value )">
 			</div>
 			<div id="bars">
 				<canvas id="barCanvas" height="1000"></canvas>
